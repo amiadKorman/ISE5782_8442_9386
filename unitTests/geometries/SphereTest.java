c65c50cb99615ac9b5b6e333ac05dcbacfc6c91e
AmiadKorman <amiadk16@gmaill.com>
2022-03-24 00:01:23 +0200

    Sphere and Triangle tests

--- unitTests/geometries/SphereTest.java
+++ unitTests/geometries/SphereTest.java
@@ -5,11 +5,12 @@ import primitives.*;
 
 import java.util.List;
 
-import static primitives.Util.*;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class SphereTest {
+
+    private Intersectable Sphere;
+    Sphere sphere = new Sphere(new Point (1, 0, 0), 1d);
 
     /**
      * Test method for {@link geometries.Sphere#getNormal(primitives.Point)}.
@@ -24,19 +25,26 @@ class SphereTest {
                 "ERROR: The calculation of normal to the Sphere is not calculated correctly");
     }
 
+    @Test
+    void allFindIntersectionality(){
+        testFindIntersections1();
+        testFindIntersections2();
+    }
+
     /**
-     * Test method for {@link geometries.Sphere#findIntersections(primitives.Ray)}.
+     * Test methods for {@link geometries.Sphere#findIntersections(primitives.Ray)}.
      */
     @Test
-    public void testFindIntersections() {
-        Sphere sphere = new Sphere(new Point (1, 0, 0), 1d);
-
+    public void testFindIntersections1() {
         // ============ Equivalence Partitions Tests ==============
 
         // TC01: Ray's line is outside the sphere (0 points)
         assertNull(sphere.findIntersections(new Ray(new Point(-1, 0, 0), new Vector(1, 1, 0))),
                 "Ray's line out of sphere");
+    }
 
+    @Test
+    public void testFindIntersections2(){
         // TC02: Ray starts before and crosses the sphere (2 points)
         Point p1 = new Point(0.0651530771650466, 0.355051025721682, 0);
         Point p2 = new Point(1.53484692283495, 0.844948974278318, 0);
@@ -48,11 +56,6 @@ class SphereTest {
             result = List.of(result.get(1), result.get(0));
         assertEquals(List.of(p1, p2), result,
                 "Ray crosses sphere");
-
-        // TC03: Ray starts inside the sphere (1 point)
-
-        // TC04: Ray starts after the sphere (0 points)
-
 
         // =============== Boundary Values Tests ==================
 
@@ -76,6 +79,116 @@ class SphereTest {
         // **** Group: Special cases
         // TC19: Ray's line is outside, ray is orthogonal to ray start to sphere's center line
 
+    }
+
+    @Test
+    void findIntersections3(){
+        // TC03: Ray starts inside the sphere (1 point)
+        Ray ray = new Ray( new Point(0.5,0,0),new Vector(1.5,0,0));
+        assertEquals(List.of(new Point(2,0,0)), Sphere.findIntersections(ray));
+    }
+
+
+    @Test
+    void findIntersections4(){
+        // TC04: Ray starts after the sphere (0 points)
+        Ray ray = new Ray(new Point(3,0,0), new Vector(1,0,0));
+        assertNull(Sphere.findIntersections(ray));
+    }
+
+    // =============== Boundary Values Tests ==================//
+
+    // **** Group: Ray's line crosses the sphere (but not the center)
+
+    @Test
+    void findIntersections5(){
+        // TC11: Ray starts at sphere and goes inside (1 points)
+        Ray ray = new Ray(new Point(0,0,0), new Vector(2,2,0));
+        assertEquals(List.of(new Point(1,1,0)) ,Sphere.findIntersections(ray));
+    }
+
+
+    @Test
+    void findIntersections6(){
+        // TC12: Ray starts at sphere and goes outside (0 points)
+        Ray ray = new Ray(new Point(1,1,0), new Vector(2,2,0));
+        assertNull(Sphere.findIntersections(ray));
+    }
+
+    // **** Group: Ray's line goes through the center
+    @Test
+    void findIntersections7(){
+        // TC13: Ray starts before the sphere (2 points)
+        Ray ray = new Ray(new Point(0,-2,0), new Vector(2,2,0));
+        var result = Sphere.findIntersections(ray);
+        assertEquals(2, result.size());
+    }
+
+
+    @Test
+    void findIntersections8() {
+        // TC14: Ray starts at sphere and goes inside (1 points)
+        Ray ray = new Ray(new Point(2, 0, 0), new Vector(-1, 0, 0));
+        assertEquals(List.of(new Point(0, 0, 0)), Sphere.findIntersections(ray));
+    }
+
+    @Test
+    void findIntersections9() {
+        // TC15: Ray starts inside (1 points)
+        Ray ray = new Ray( new Point(0.59, 0, 0), new Vector(-0.59, 0, 0));
+        assertEquals(List.of(new Point(0, 0, 0)), Sphere.findIntersections(ray));
+    }
+
+    @Test
+    void findIntersections10(){
+        // TC16: Ray starts at the center (1 points)
+        Ray ray = new Ray(new Point(1,0,0), new Vector(2,2,0));
+        assertThrows(IllegalArgumentException.class, ()-> Sphere.findIntersections(ray));
+    }
+
+    @Test
+    void findIntersections11(){
+        // TC17: Ray starts at sphere and goes outside (0 points)
+        Ray ray = new Ray(new Point(2,0,0), new Vector(2,0,0));
+        assertNull(Sphere.findIntersections(ray));
+    }
+
+    @Test
+    void findIntersections12(){
+        // TC18: Ray starts after sphere (0 points)
+        Ray ray = new Ray (new Point(3,0,0), new Vector(1,0,0));
+        assertNull(Sphere.findIntersections(ray));
+    }
+
+    // **** Group: Ray's line is tangent to the sphere (all tests 0 points)
+    @Test
+    void findIntersections13(){
+        // TC19: Ray starts before the tangent point
+        Ray ray = new Ray( new Point(2,1,1), new Vector(-1,-1,0));
+        assertNull(Sphere.findIntersections(ray));
+    }
+
+    @Test
+    void findIntersections14(){
+        // TC20: Ray starts at the tangent point
+        Ray ray = new Ray(new Point(1,0,1), new Vector(1,1,0));
+        assertNull(Sphere.findIntersections(ray));
+    }
+
+
+    @Test
+    void findIntersections15(){
+        // TC21: Ray starts after the tangent point
+        Ray ray = new Ray(new Point(2,1,1), new Vector(4,4,0));
+        assertNull(Sphere.findIntersections(ray));
+    }
+
+    // **** Group: Special cases
+    @Test
+    void findIntersections16(){
+        // TC22: Ray's line is outside, ray is orthogonal to ray start to sphere's center line
+        Ray ray = new Ray(new Point(3,0,0), new Vector(0,0,1));
+        assertNull(Sphere.findIntersections(ray));
     }
 
 }
--- unitTests/geometries/TriangleTest.java
+++ unitTests/geometries/TriangleTest.java
@@ -1,8 +1,9 @@
 package geometries;
 
 import org.junit.jupiter.api.Test;
-import primitives.Point;
-import primitives.Vector;
+import primitives.*;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,7 +27,85 @@ class TriangleTest {
     }
 
     @Test
-    void testFindIntersections() {
-        fail("not implemented yet!");
+    void allFindIntersectionality(){
+        testFindIntersections1();
+        testFindIntersections2();
     }
+
+    /**
+     *tests for {@link geometries.Triangle#findIntersections(primitives.Ray)}.
+     */
+    // ============ Equivalence Partitions Tests ==============
+    //TC01: Inside polygon/triangle(1 Point)
+    @Test
+    void findIntersections1() {
+        Triangle triangle = new Triangle(
+                new Point(2, 0, 0),
+                new Point(0, 3, 0),
+                new Point(0, 0, 0));
+
+        Ray ray = new Ray(new Point(0, 0, -1), new Vector(1, 1, 1));
+        List<Point> result = triangle.findIntersections(ray);
+        Point p1 = new Point(1, 1, 0);
+        assertEquals(List.of(p1), result, "Inside polygon/triangle(1 Point)");
+    }
+
+    //TC02: Outside against edge(0 Point)
+    @Test
+    void findIntersections2() {
+        Triangle triangle = new Triangle(
+                new Point(2, 0, 0),
+                new Point(0, 3, 0),
+                new Point(0, 0, 0));
+        Ray ray = new Ray(new Point(0, 0, -1), new Vector(2, 1, 1));
+        assertNull(triangle.findIntersections(ray), "Outside against edge");
+    }
+
+    //TC03: Outside against vertex(0 Point)
+    @Test
+    void findIntersections3() {
+        Triangle triangle = new Triangle(
+                new Point(2, 0, 0),
+                new Point(0, 3, 0),
+                new Point(0, 0, 0));
+        Ray ray = new Ray(new Point(0, 0, -1), new Vector(3, -0.5, 1));
+        assertNull(triangle.findIntersections(ray), "Outside against vertex");
+    }
+
+    // =============== Boundary Values Tests ==================
+    //****Three cases (the ray begins "before" the plane)**
+
+    //TC11: On edge(0 Point)
+    @Test
+    void findIntersections4() {
+        Triangle triangle = new Triangle(
+                new Point(2, 0, 0),
+                new Point(0, 3, 0),
+                new Point(0, 0, 0));
+        Ray ray = new Ray(new Point(0, 0, -1),new Vector(0, 1, 1));
+        assertNull(triangle.findIntersections(ray), "On edge");
+    }
+
+    //TC12: In vertex(0 Point)
+    @Test
+    void findIntersections5() {
+        Triangle triangle = new Triangle(
+                new Point(2, 0, 0),
+                new Point(0, 3, 0),
+                new Point(0, 0, 0));
+        Ray ray = new Ray(new Point(0, 0, -1), new Vector(0, 3, 1));
+        assertNull(triangle.findIntersections(ray), "In vertex");
+    }
+
+    //TC13: On edge's continuation(0 Point)
+    @Test
+    void findIntersections6() {
+        Triangle triangle = new Triangle(
+                new Point(2, 0, 0),
+                new Point(0, 3, 0),
+                new Point(0, 0, 0));
+        Ray ray6 = new Ray(new Point(0, 0, -1), new Vector(0, 4, 1));
+        assertNull(triangle.findIntersections(ray6), "On edge's continuation");
+    }
+
 }
